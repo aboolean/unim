2e1549257ef16dcd9e5f7528a3f403b99d3d5841
Andre Aboulian <aandre@mit.edu>
2014-03-30 02:21:50 -0400

    main app added

--- django/unim/settings.py
+++ django/unim/settings.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'android',
 )
 
 # A sample logging configuration. The only tangible logging
